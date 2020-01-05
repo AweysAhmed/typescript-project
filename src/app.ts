64d1f05dd972452cb38ee968c6f58ad1dc94b5d2
OFFICE <office@OFFICEs-MacBook-Air.local>
2020-01-05 01:19:32 -0500

    worked on basic input validation

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,40 @@
+//validation
+interface Validator {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validateInput: Validator) {
+  let isValid = true;
+  if (validateInput.required) {
+    isValid = isValid && validateInput.value.toString().trim().length !== 0;
+  }
+  if (
+    validateInput.minLength != null &&
+    typeof validateInput.value === "string"
+  ) {
+    isValid = isValid && validateInput.value.length >= validateInput.minLength;
+  }
+  if (
+    validateInput.maxLength != null &&
+    typeof validateInput.value === "string"
+  ) {
+    isValid = isValid && validateInput.value.length <= validateInput.maxLength;
+  }
+  if (validateInput.min != null && typeof validateInput.value === "number") {
+    isValid = isValid && validateInput.value >= validateInput.min;
+  }
+
+  if (validateInput.max != null && typeof validateInput.value === "number") {
+    isValid = isValid && validateInput.value <= validateInput.max;
+  }
+  return isValid;
+}
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -35,9 +72,54 @@ class ProjectInput {
     this.attach();
   }
 
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDesc = this.descInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    const titleValidate: Validator = {
+      value: enteredTitle,
+      required: true
+    };
+    const descValidate: Validator = {
+      value: enteredDesc,
+      required: true,
+      minLength: 5
+    };
+    const peopleValidate: Validator = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5
+    };
+
+    if (
+      !validate(titleValidate) ||
+      !validate(descValidate) ||
+      !validate(peopleValidate)
+    ) {
+      alert("Invalid input");
+      return;
+    } else {
+      return [enteredTitle, enteredDesc, +enteredPeople];
+    }
+  }
+
+  private clearInput() {
+    this.titleInputElement.value = "";
+    this.descInputElement.value = "";
+    this.peopleInputElement.value = "";
+  }
+
   private submitHandler(event: Event) {
     event.preventDefault();
     console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+      this.clearInput();
+    }
   }
 
   private configure() {
